2b3f49fbcb5e925e36aa98e2b13cd69af416eb63
Temirkhan Myrzamadi <hirrolot@gmail.com>
2020-10-19 19:37:58 +0600

    Refactor eval/args.h

--- include/macrolop/eval/args.h
+++ include/macrolop/eval/args.h
@@ -6,9 +6,10 @@
 #include "rec/control.h"
 #include "term.h"
 
-#define MACROLOP_PRIVATE_EVAL_ARGS_HOOK()               MACROLOP_PRIVATE_EVAL_ARGS
-#define MACROLOP_PRIVATE_EVAL_ARGS_AUX_HOOK()           MACROLOP_PRIVATE_EVAL_ARGS_AUX
-#define MACROLOP_PRIVATE_EVAL_ARGS_SAVE_CALL_RES_HOOK() MACROLOP_PRIVATE_EVAL_ARGS_SAVE_CALL_RES
+#define MACROLOP_PRIVATE_EVAL_ARGS_HOOK()     MACROLOP_PRIVATE_EVAL_ARGS
+#define MACROLOP_PRIVATE_EVAL_ARGS_AUX_HOOK() MACROLOP_PRIVATE_EVAL_ARGS_AUX
+#define MACROLOP_PRIVATE_EVAL_ARGS_SAVE_IN_ACC_AND_CONTINUE_HOOK()                                 \
+    MACROLOP_PRIVATE_EVAL_ARGS_SAVE_IN_ACC_AND_CONTINUE
 
 #define MACROLOP_PRIVATE_EVAL_ARGS(k, k_cx, ...)                                                   \
     MACROLOP_PRIVATE_EVAL_ARGS_AUX(                                                                \
@@ -21,13 +22,7 @@
 
 #define MACROLOP_PRIVATE_EVAL_ARGS_AUX_trivial_call MACROLOP_PRIVATE_EVAL_ARGS_AUX_CALL
 #define MACROLOP_PRIVATE_EVAL_ARGS_AUX_call         MACROLOP_PRIVATE_EVAL_ARGS_AUX_CALL
-
-#define MACROLOP_PRIVATE_EVAL_ARGS_AUX_v(k, k_cx, acc, tail, ...)                                  \
-    MACROLOP_PRIVATE_EVAL_REC_CONTINUE(                                                            \
-        MACROLOP_PRIVATE_EVAL_ARGS_AUX_HOOK, (k, k_cx),                                            \
-        MACROLOP_PRIVATE_EVAL_AUX_EXTEND_PARENTHESISED(                                            \
-            acc, __VA_ARGS__ MACROLOP_PRIVATE_EVAL_ARGS_COMMA_OR_EMPTY(tail)),                     \
-        MACROLOP_PRIVATE_EVAL_AUX_UNPARENTHESISE(tail))
+#define MACROLOP_PRIVATE_EVAL_ARGS_AUX_v            MACROLOP_PRIVATE_EVAL_ARGS_SAVE_IN_ACC_AND_CONTINUE
 
 #define MACROLOP_PRIVATE_EVAL_ARGS_AUX_end(k, k_cx, acc, _tail, _)                                 \
     MACROLOP_PRIVATE_EVAL_REC_CONTINUE(k, k_cx, MACROLOP_PRIVATE_EVAL_AUX_UNPARENTHESISE(acc))
@@ -35,10 +30,15 @@
 #define MACROLOP_PRIVATE_EVAL_ARGS_AUX_CALL(k, k_cx, acc, tail, op, ...)                           \
     MACROLOP_PRIVATE_EVAL_REC_CONTINUE(                                                            \
         MACROLOP_PRIVATE_EVAL_AUX_HOOK,                                                            \
-        (MACROLOP_PRIVATE_EVAL_ARGS_SAVE_CALL_RES_HOOK, (k, k_cx, acc, tail)),                     \
+        (MACROLOP_PRIVATE_EVAL_ARGS_SAVE_IN_ACC_AND_CONTINUE_HOOK, (k, k_cx, acc, tail)),          \
         call(op, __VA_ARGS__))
 
-#define MACROLOP_PRIVATE_EVAL_ARGS_SAVE_CALL_RES MACROLOP_PRIVATE_EVAL_ARGS_AUX_v
+#define MACROLOP_PRIVATE_EVAL_ARGS_SAVE_IN_ACC_AND_CONTINUE(k, k_cx, acc, tail, ...)               \
+    MACROLOP_PRIVATE_EVAL_REC_CONTINUE(                                                            \
+        MACROLOP_PRIVATE_EVAL_ARGS_AUX_HOOK, (k, k_cx),                                            \
+        MACROLOP_PRIVATE_EVAL_AUX_EXTEND_PARENTHESISED(                                            \
+            acc, __VA_ARGS__ MACROLOP_PRIVATE_EVAL_ARGS_COMMA_OR_EMPTY(tail)),                     \
+        MACROLOP_PRIVATE_EVAL_AUX_UNPARENTHESISE(tail))
 
 #define MACROLOP_PRIVATE_EVAL_ARGS_COMMA_OR_EMPTY(tail)                                            \
     MACROLOP_PRIVATE_EVAL_AUX_IF(                                                                  \
