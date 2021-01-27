bd83eeebf57a34ee6dce38ccd4877da3eb65cb72
Temirkhan Myrzamadi <hirrolot@gmail.com>
2021-01-27 15:19:44 +0600

    Refactor examples/match_string.c

--- examples/match_string.c
+++ examples/match_string.c
@@ -11,30 +11,20 @@
 
 #include <epilepsy.h>
 
-#define MATCH(str, ...)                                                                            \
-    { E_eval(GEN_ALL_BRANCHES(v(str), E_list(v(__VA_ARGS__)))) out:; }
+#define MATCH(str, ...) MATCH_AUX(str, E_eval(E_list(v(__VA_ARGS__))))
 
-// Desugaring {
-#define GEN_ALL_BRANCHES(str, branches)   E_call(GEN_ALL_BRANCHES, str branches)
-#define GEN_BRANCH(str, ...)              E_call(GEN_BRANCH, str __VA_ARGS__)
-#define GEN_BRANCH_AUX(str, pattern, ...) E_call(GEN_BRANCH_AUX, str pattern __VA_ARGS__)
-#define GEN_DEFAULT_BRANCH(branches)      E_call(GEN_DEFAULT_BRANCH, branches)
-// }
+#define MATCH_AUX(str, branches)                                                                   \
+    { GEN_BRANCHES(str, branches) GEN_DEFAULT_BRANCH(branches) out:; }
 
-// Implementation {
-#define GEN_ALL_BRANCHES_IMPL(str, branches)                                                       \
-    E_listUnwrap(E_listMap(                                                                        \
-        E_compose(E_appl(v(GEN_BRANCH), v(str)), v(E_unparenthesiseUnevaluated)),                  \
-        E_listInit(v(branches))))                                                                  \
-                                                                                                   \
-        GEN_DEFAULT_BRANCH(v(branches))
+#define GEN_BRANCHES(str, branches)                                                                \
+    E_listEval(E_listMap(E_appl(v(GEN_BRANCH), v(str)), E_listInit(v(branches))))
 
-#define GEN_BRANCH_IMPL(str, ...) GEN_BRANCH_AUX(v(str), v(__VA_ARGS__))
+#define GEN_BRANCH_IMPL(str, branch)                                                               \
+    E_call(GEN_BRANCH_AUX, v(str) E_unparenthesiseUnevaluated(v(branch)))
 #define GEN_BRANCH_AUX_IMPL(str, pattern, ...)                                                     \
     v(if (strcmp(str, pattern) == 0) { __VA_ARGS__ goto out; })
 
-#define GEN_DEFAULT_BRANCH_IMPL(branches) E_unparenthesiseUnevaluated(E_listLast(v(branches)))
-// }
+#define GEN_DEFAULT_BRANCH(branches) E_eval(E_unparenthesiseUnevaluated(E_listLast(v(branches))))
 
 // Arity specifiers {
 #define GEN_BRANCH_ARITY 2
